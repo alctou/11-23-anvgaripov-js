d1bb79d47235b490d8a0f36450f8d67286dcb4bb
Гарипов Анвар Ильназович <152308@mck-ktits.ru>
2024-09-21 09:17:23 +0300

    make 2 practic

---
+++ sm2/asset/js/main.js
@@ -0,0 +1,15 @@
+prompt('Как тебя зовут?')
+let age = prompt('Сколько тебе лет?', 100);
+if (age < 20){
+     alert('Тебе меньше 20')
+} else{
+    alert('Привет')
+    alert('Привет')
+    alert('Привет')
+    alert('Привет')
+    alert('Привет')
+}
+let teach = confirm('Хочешь учить js?')
+if(true){
+    alert('Супер!')
+}
